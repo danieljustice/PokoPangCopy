f999a3f8a289287a371a15f3b2d9acb2264f12ed
danieljustice <danieljustice09@gmail.com>
2018-01-11 00:19:52 -0500

    hexcoordinates working and being displayed nicely in editor

---
+++ Assets/Editor/HexCoordinatesDrawer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+//Nice little editor script to make the HexCoordinates of a Cell visible in the editor
+//without them being editable. No Touchy.
+[CustomPropertyDrawer(typeof(HexCoordinates))]
+public class HexCoordinatesDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        HexCoordinates coordinates = new HexCoordinates(
+            property.FindPropertyRelative("x").intValue,
+            property.FindPropertyRelative("z").intValue
+            );
+        position = EditorGUI.PrefixLabel(position, label);
+        GUI.Label(position, coordinates.ToString());
+    }
+}
